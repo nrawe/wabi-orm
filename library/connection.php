011b1d9fae7f281f884268eb3fe90aeb6023ac60
Nick Rawe <nrawe@reapit.com>
2019-02-09 18:00:18 +0000

    feat: establish core of connect

--- library/connection.php
+++ library/connection.php
@@ -3,4 +3,19 @@ declare(strict_types=1);
 
 namespace WabiORM;
 
+use PDO;
+use PDOStatement;
+
+/**
+ * Decorates a PDO connection to facilitate middleware during query execution.
+ */
+function connect(PDO $connection): callable {
+    return function (string $query, array $params = []) use ($connection): PDOStatement {
+        $statement = $connection->prepare($query);
+        $statement->execute($params);
+
+        return $statement;
+    };
+}
+
 
--- spec/connection.spec.php
+++ spec/connection.spec.php
@@ -1,3 +1,23 @@
 <?php
 
+use PDO;
+use PDOStatement;
+use function Eloquent\Phony\Kahlan\on;
+use function WabiORM\connect;
 
+describe('connection', function () {
+    beforeEach(function (PDO $db, PDOStatement $stmt) {
+        $this->db = $db;
+        $this->stmt = $stmt; 
+    });
+
+    describe('connect()', function () {
+        it('provides a minimal interface over PDO', function () {
+            on($this->db)->prepare->with('query')->returns($this->stmt);
+
+            $execute = connect($this->db);
+
+            expect($execute('query'))->toBeAnInstanceOf('PDOStatement');
+        });
+    });
+});
