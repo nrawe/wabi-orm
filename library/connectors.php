55a4efbeeee4b6717013b86f66e367796d177cfb
Nick Rawe <nrawe@reapit.com>
2019-02-12 21:12:05 +0000

    feat: add mysql connectors

---
+++ library/connectors.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This subpackage contains some base connection factories for convenience.
+ */
+namespace WabiORM;
+
+use PDO;
+
+/**
+ * A basic MySQL connection factory.
+ *
+ * @subpackage WabiORM.Connectors
+ * @param string $host
+ * @param string $db
+ * @param string $user
+ * @param string $pwd
+ * @return \PDO
+ */
+function mysql(string $host, string $db, string $user, string $pwd): PDO {
+    $dsn = 'mysql:host='.$host.';dbname='.$schema.';';
+    $options = [
+        PDO::ATTR_CASE => PDO::CASE_LOWER,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ];
+
+    return new PDO($dsn, $user, $pwd, $options);
+}
