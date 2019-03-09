23c630491a036a8c6d69c33b03fd0ca0f395f0de
Nick Rawe <nrawe@reapit.com>
2019-03-09 21:55:45 +0000

    feat: add SQLite connector

--- library/connectors.php
+++ library/connectors.php
@@ -12,6 +12,7 @@ use PDO;
  * A basic MySQL connection factory.
  *
  * @subpackage WabiORM.Connectors
+ * @see https://secure.php.net/manual/en/ref.pdo-mysql.connection.php
  * @param string $host The hostname of the database server
  * @param string $db The schema name
  * @param string $user The username for authentication
@@ -20,10 +21,31 @@ use PDO;
  */
 function mysql(string $host, string $db, string $user, string $pwd): PDO {
     $dsn = 'mysql:host='.$host.';dbname='.$db.';';
-    $options = [
+
+    return new PDO($dsn, $user, $pwd, pdo_options());
+}
+
+/**
+ * A basic SQLite connection factory.
+ *
+ * @subpackage WabiORM.Connectors
+ * @see https://secure.php.net/manual/en/ref.pdo-sqlite.connection.php
+ * @param string $file The path to the file containing the database
+ * @return \PDO
+ */
+function sqlite(string $file = ':memory:'): PDO {
+    return new PDO('sqlite:' . $file, '', '', pdo_options());
+}
+
+/**
+ * Returns the options for PDO to work with the connectors.
+ *
+ * @subpackage WabiORM.Connectors
+ * @return array
+ */
+function pdo_options(): array {
+    return [
         PDO::ATTR_CASE => PDO::CASE_LOWER,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ];
-
-    return new PDO($dsn, $user, $pwd, $options);
 }
